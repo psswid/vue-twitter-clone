d247c0f0478aac4c4c6c230e7f0a696145d7b45e
psswid <psswiderski@gmail.com>
2020-10-18 20:08:26 +0200

    basic single file component - basic usage of watchers, computed prop and lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    {{fullName}}
+    <strong>Followers: </strong>{{ followers }}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "",
+        firstName: "Roman",
+        lastName: "Motopompa",
+        email: "",
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    /**
+     * name of watcher has to be the same as value he observes
+     */
+    followers(newFollowerCount, oldFollowerCount) {
+      console.log(oldFollowerCount, newFollowerCount)
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} gained a follower!`);
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }  
+  },
+  methods: {
+    followUser(){
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
 }
 </script>
 
@@ -19,8 +57,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
