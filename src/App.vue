53f2869b05efdfe04ea107deae38f2667e63fa18
psswid <psswiderski@gmail.com>
2020-10-18 20:20:13 +0200

    code cleanup, moved component logic to antother file

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,18 @@
 <template>
   <div id="app">
-    {{fullName}}
-    <strong>Followers: </strong>{{ followers }}
-    <button @click="followUser">
-      Follow
-    </button>
+    <UserProfile/>
   </div>
 </template>
 
 <script>
 
+import UserProfile from "./components/UserProfile";
+
 export default {
   name: 'App',
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: "",
-        firstName: "Roman",
-        lastName: "Motopompa",
-        email: "",
-        isAdmin: true
-      }
-    }
-  },
-  watch: {
-    /**
-     * name of watcher has to be the same as value he observes
-     */
-    followers(newFollowerCount, oldFollowerCount) {
-      console.log(oldFollowerCount, newFollowerCount)
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} gained a follower!`);
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }  
-  },
-  methods: {
-    followUser(){
-      this.followers++;
-    }
-  },
-  mounted() {
-    this.followUser();
-  },
+  components: {
+    UserProfile
+  }
 }
 </script>
 
