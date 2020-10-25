712f90ac4f36a665bd670dd456e528a4bd253e85
psswid <psswiderski@gmail.com>
2020-10-25 17:48:26 +0100

    add global styling

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  background-color: #f3f5fa;
 }
 </style>
